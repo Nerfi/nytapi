60611c67f0727e9afd61d8ed0f4a98177075eee3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-19 13:07:27 +0200

    .

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+
+  def new
+
+  end
+
+  def show
+    search_term = params['q'].capitalize
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/search', to: 'searches#new'
+  post '/search', to: 'search#show'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
