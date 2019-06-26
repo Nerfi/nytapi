00c2e6e0190625ed05249ac77347f8aadb14b334
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-26 14:59:35 +0200

    no clue why i got bad URI

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,18 +3,37 @@ class SearchesController < ApplicationController
     require 'json'
     require 'open-uri'
 
-    # url = 'https://api.nytimes.com/svc/topstories/v2/travel.json'
-     url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
+     #url = 'https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}'
+    # url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
 
-  user_serialized = open(url).read
-  user = JSON.parse(user_serialized)
+  #user_serialized = open(url).read
+  #user = JSON.parse(user_serialized)
 
   def new
 
   end
 
   def show
+
+   url = 'https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}'
+    # url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
+
+  user_serialized = open(url).read
+  user = JSON.parse(user_serialized)
+
+
+
+
+
     search_term = params['q'].capitalize
+
+    if !json['total'].zero?
+      @articles = json["articles"]
+    else
+      render :new
+    end
+
+
 
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/search', to: 'searches#new'
-  post '/search', to: 'search#show'
+  post '/search', to: 'searches#show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
