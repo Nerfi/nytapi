894f476878944d9845b92314a29e410a3571322d
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-27 19:27:49 +0200

    not workign and no clue

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,7 @@ class SearchesController < ApplicationController
     require 'json'
     require 'open-uri'
 
+
      #url = 'https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}'
     # url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
 
@@ -15,11 +16,21 @@ class SearchesController < ApplicationController
 
   def show
 
-   url = 'https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}'
-    # url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
+   # @articles = URL.parse(url.encode(url))
+
+   #uri = URI('https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}')
+    url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku'
+
+
+
+
 
   user_serialized = open(url).read
+
+
+
   user = JSON.parse(user_serialized)
+  @articles = user
 
 
 
@@ -27,8 +38,8 @@ class SearchesController < ApplicationController
 
     search_term = params['q'].capitalize
 
-    if !json['total'].zero?
-      @articles = json["articles"]
+    if !JSON[@articles].nil?
+      @articles = JSON[@articles]
     else
       render :new
     end
