7e4fa05eaa86a21562d1629df4f114598ffc978c
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-28 20:50:48 +0200

    401

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -14,14 +14,16 @@ class SearchesController < ApplicationController
 
   def show
 #puede que en la linea 17 tengamos que cambiar las dobles comillas por singles
-   url = 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=#{ENV["NEW_YORk_TIMES_PUBLISHABLE_KEY"]}'
+   url = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=#{ENV['NEW_YORk_TIMES_PUBLISHABLE_KEY']}"
 
   articles = open(url).read
   articles = JSON.parse(articles)
    # @articles = URL.parse(url.encode(url))
 
+#code not workign and maybe not useful
+   #uri = URI.parse("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=#{ENV['NEW_YORk_TIMES_PUBLISHABLE_KEY']}")
    #uri = URI('https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=T8XwJbOsNplMIhKOCym4I5ECLLGX73ku&medium=#{search_term}')
-
+   #uri.open
 
 
   #user_serialized = open(url).read
@@ -38,11 +40,11 @@ class SearchesController < ApplicationController
 
     search_term = params['q'].capitalize
 
-    if !JSON[@articles].nil?
-      @articles = JSON[@articles]
-    else
-      render :new
-    end
+      if !JSON[@articles].nil?
+        @articles = JSON[@articles]
+      else
+        render :new
+      end
 
 
 
